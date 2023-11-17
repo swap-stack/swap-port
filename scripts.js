7505f2076b41b92d3ff3e98ebba3fd352a1c4a9c
Swapnila Kavishwar <62277537+swap-stack@users.noreply.github.com>
2023-11-17 17:50:27 +0530

    updated jquery code

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
-// Simulated project data
-const projects = [
-    { title: "Project 1", description: "Description of Project 1" },
-    { title: "Project 2", description: "Description of Project 2" },
-    // Add more projects as needed
-];
+// // Simulated project data
+// const projects = [
+//     { title: "Project 1", description: "Description of Project 1" },
+//     { title: "Project 2", description: "Description of Project 2" },
+//     // Add more projects as needed
+// ];
 
-// Function to display projects dynamically
-function displayProjects() {
-    const projectList = document.querySelector('.project-list');
-    projects.forEach(project => {
-        const projectItem = document.createElement('div');
-        projectItem.classList.add('project');
-        projectItem.innerHTML = `<h3>${project.title}</h3><p>${project.description}</p>`;
-        projectList.appendChild(projectItem);
-    });
-}
+// // Function to display projects dynamically
+// function displayProjects() {
+//     const projectList = document.querySelector('.project-list');
+//     projects.forEach(project => {
+//         const projectItem = document.createElement('div');
+//         projectItem.classList.add('project');
+//         projectItem.innerHTML = `<h3>${project.title}</h3><p>${project.description}</p>`;
+//         projectList.appendChild(projectItem);
+//     });
+// }
 
-// Call the function to display projects when the page loads
-window.onload = function () {
-    displayProjects();
-};
+// // Call the function to display projects when the page loads
+// window.onload = function () {
+//     displayProjects();
+// };
 
 /*-----
 Spanizer
